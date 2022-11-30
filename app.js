4a3110755e129dfb3ff19ee9b75d6a8efff7905c
MrdKongolo <medardbaraka09@gmail.com>
2022-11-30 15:11:53 +0200

    last commit

--- app.js
+++ app.js
@@ -33,78 +33,99 @@
 // });
 
 /** Delete book-list */
-const list = document.querySelector('#book-list ul');
+document.addEventListener('DOMContentLoaded', function(){
 
-list.addEventListener('click', function(e){
-  if(e.target.className=='delete'){
-    const li = e.target.parentElement;
-    list.removeChild(li);
-  }
-});
-
-// prevent default behaviour
-
-const addForm = document.forms['add-book'];
-
-addForm.addEventListener('submit', function(e){
-  e.preventDefault();
-  const value = addForm.querySelector('input[type="text"]').value;
- 
-  // Create elements
-
-  const li = document.createElement('li');
-  const bookName = document.createElement('span');
-  const deleteBtn = document.createElement('span');
-
-  //Add Content
-  deleteBtn.textContent = 'Supprimer';
-  bookName.textContent = value;
-
-  // Add Classes
-  bookName.classList.add('name');
-  deleteBtn.classList.add('delete');
-
-  // Append to document
-  li.appendChild(bookName);
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-});
-
-
-// Hide books
-
-const hideBox = document.querySelector('#hide');
-
-hideBox.addEventListener('change', function(e){
-  if(hideBox.checked){
-    list.style.display = 'none';
-  } else {
-    list.style.display = 'initial';
-  }
-});
-
-// Filter books
-const search = document.forms['search-books'].querySelector('input');
-
-search.addEventListener('keyup', function(e){
-
-  const term = e.target.value.toLowerCase();
-  const books = list.getElementsByTagName('li');
-
-  Array.from(books).forEach(function(book) {
-
-    const title = book.firstElementChild.textContent;
-
-    if(title.toLowerCase().indexOf(term) != -1) {
-
-      book.style.display = 'block';
-
-    } else {
-
-      book.style.display = 'none';
-
+  const list = document.querySelector('#book-list ul');
+  
+  list.addEventListener('click', function(e){
+    if(e.target.className=='delete'){
+      const li = e.target.parentElement;
+      list.removeChild(li);
     }
-
   });
-
-});
+  
+  // prevent default behaviour
+  
+  const addForm = document.forms['add-book'];
+  
+  addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+   
+    // Create elements
+  
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+  
+    //Add Content
+    deleteBtn.textContent = 'Supprimer';
+    bookName.textContent = value;
+  
+    // Add Classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+  
+    // Append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+  });
+  
+  
+  // Hide books
+  
+  const hideBox = document.querySelector('#hide');
+  
+  hideBox.addEventListener('change', function(e){
+    if(hideBox.checked){
+      list.style.display = 'none';
+    } else {
+      list.style.display = 'initial';
+    }
+  });
+  
+  // Filter books
+  const search = document.forms['search-books'].querySelector('input');
+  
+  search.addEventListener('keyup', function(e){
+  
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+  
+    Array.from(books).forEach(function(book) {
+  
+      const title = book.firstElementChild.textContent;
+  
+      if(title.toLowerCase().indexOf(term) != -1) {
+  
+        book.style.display = 'block';
+  
+      } else {
+  
+        book.style.display = 'none';
+  
+      }
+  
+    });
+  
+  });
+  
+  //tabbed content
+  
+  const tabs = document.querySelector('.tabs');
+  const panels = document.querySelectorAll('.panel');
+  tabs.addEventListener('click', function(e){
+    if(e.target.tagName == "LI") {    
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      panels.forEach(function(panel){
+        if(panel == targetPanel){
+          panel.classList.add('active');
+        }else {
+          panel.classList.remove('active');
+        }
+      })
+    }
+  
+  })
+})
